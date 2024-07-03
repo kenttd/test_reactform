504f76da00b16c2c26f08d1b795bffbaecd7e582
Kent Travis Donovan <kent@Kents-MacBook-Air.local>
2024-07-03 10:03:11 +0700

    dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { RegisterForm } from "@/components/form";
+import { ModeToggle } from "@/components/toggleDarkMode";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <RegisterForm />
+      <ModeToggle />
     </main>
   );
 }
